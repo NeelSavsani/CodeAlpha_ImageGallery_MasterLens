21083365532ae342fc3798857b51565dff26a10e
NeelSavsani <neelsavsani7@gmail.com>
2026-05-04 17:23:50 +0530

    Added arrow buttons in enlarged image

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+let images = document.querySelectorAll(".gallery img");
+let currentIndex = 0;
+
 function openImage(img) {
     const preview = document.getElementById("preview");
     const previewImg = document.getElementById("preview-img");
 
     preview.style.display = "flex";
     previewImg.src = img.src;
+
+    // store index
+    images.forEach((image, index) => {
+        if (image === img) {
+            currentIndex = index;
+        }
+    });
 }
 
 function closeImage() {
     document.getElementById("preview").style.display = "none";
 }
 
-// ESC key support
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById("preview-img").src = images[currentIndex].src;
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    document.getElementById("preview-img").src = images[currentIndex].src;
+}
+
+// Keyboard support
 document.addEventListener("keydown", function(e) {
-    if (e.key === "Escape") {
-        closeImage();
+    const preview = document.getElementById("preview");
+
+    if (preview.style.display === "flex") {
+        if (e.key === "ArrowRight") nextImage();
+        if (e.key === "ArrowLeft") prevImage();
+        if (e.key === "Escape") closeImage();
     }
 });
